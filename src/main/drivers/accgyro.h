KingNeeO/betaflight

Apply Gyro Filter on EXTI // Separate GYRO from PID task

src/main/drivers/accgyro.h
@@ -25,9 +25,8 @@ typedef struct gyro_s {
     sensorGyroInitFuncPtr init;                             // initialize function
     sensorReadFuncPtr read;                                 // read 3 axis data function
     sensorReadFuncPtr temperature;                          // read temperature if available
-    sensorInterruptFuncPtr intStatus;
     float scale;                                            // scalefactor
-    uint32_t targetLooptime;
+    uint32_t gyroSamplingRate;
 } gyro_t;
 
 typedef struct acc_s {

src/main/drivers/gyro_sync.c
@@ -9,19 +9,16 @@
 #include <stdint.h>
 
 #include "platform.h"
+#include "build/debug.h"
 
 #include "sensor.h"
 #include "accgyro.h"
 #include "gyro_sync.h"
+#include "system.h"
 
-static uint8_t mpuDividerDrops;
+#include "common/axis.h"
 
-bool gyroSyncCheckUpdate(const gyro_t *gyro)
-{
-    if (!gyro->intStatus)
-        return false;
-    return gyro->intStatus();
-}
+#include "sensors/gyro.h"
 
 #define GYRO_LPF_256HZ      0
 #define GYRO_LPF_188HZ      1
@@ -32,9 +29,27 @@ bool gyroSyncCheckUpdate(const gyro_t *gyro)
 #define GYRO_LPF_5HZ        6
 #define GYRO_LPF_NONE       7
 
-uint32_t gyroSetSampleRate(uint8_t lpf, uint8_t gyroSyncDenominator)
+static int mpuDividerDrops;
+static volatile bool isPidScheduledToRun;
+static int pidProcessDenom;
+uint32_t lastGyroInterruptCallDelta;
+
+bool pidScheduledToRun(void)
+{
+    bool ret;
+    if (isPidScheduledToRun) {
+        ret = true;
+        isPidScheduledToRun= false;
+    } else {
+        ret = false;
+    }
+    return ret;
+}
+
+uint32_t gyroSetSampleRate(uint8_t lpf, uint8_t gyroSyncDenominator, uint8_t pidDenom)
 {
     int gyroSamplePeriod;
+    pidProcessDenom = pidDenom;
 
     if (lpf == GYRO_LPF_256HZ || lpf == GYRO_LPF_NONE) {
         gyroSamplePeriod = 125;
@@ -52,4 +67,29 @@ uint32_t gyroSetSampleRate(uint8_t lpf, uint8_t gyroSyncDenominator)
 uint8_t gyroMPU6xxxGetDividerDrops(void)
 {
     return mpuDividerDrops;
+}
+
+void gyroHandleInterrupt(void) {
+	static int16_t gyroADCRaw[XYZ_AXIS_COUNT];
+	static int pidProcessCountDown;
+
+//#ifdef DEBUG_MPU_DATA_READY_INTERRUPT
+    static uint32_t lastGyroInterruptCallAt = 0;
+    uint32_t now = micros();
+    lastGyroInterruptCallDelta = now - lastGyroInterruptCallAt;
+    debug[0] = lastGyroInterruptCallDelta;
+    lastGyroInterruptCallAt = now;
+
+    if (!gyro.read(gyroADCRaw)) {
+        return;
+    }
+
+    processGyroData(gyroADCRaw);
+
+    if (pidProcessCountDown) {
+        pidProcessCountDown--;
+    } else {
+        pidProcessCountDown = pidProcessDenom - 1;
+        isPidScheduledToRun = true;
+    }
 }

src/main/flight/pid.c
@@ -74,7 +74,7 @@ pidControllerFuncPtr pid_controller = pidBetaflight; // which pid controller are
 
 void setTargetPidLooptime(uint8_t pidProcessDenom)
 {
-    targetPidLooptime = gyro.targetLooptime * pidProcessDenom;
+    targetPidLooptime = gyro.gyroSamplingRate * pidProcessDenom;
 }
 
 void pidResetErrorGyroState(void)

manors/betaflight_old

src/main/drivers/accgyro.h
@@ -25,9 +25,8 @@ typedef struct gyro_s {
     sensorGyroInitFuncPtr init;                             // initialize function
     sensorReadFuncPtr read;                                 // read 3 axis data function
     sensorReadFuncPtr temperature;                          // read temperature if available
-    sensorInterruptFuncPtr intStatus;
     float scale;                                            // scalefactor
-    uint32_t targetLooptime;
+    uint32_t gyroSamplingInterval;
 } gyro_t;
 
 typedef struct acc_s {

src/main/drivers/gyro_sync.c
@@ -9,32 +9,38 @@
 #include <stdint.h>
 
 #include "platform.h"
+#include "build/debug.h"
 
 #include "sensor.h"
 #include "accgyro.h"
 #include "gyro_sync.h"
+#include "system.h"
 
-static uint8_t mpuDividerDrops;
+#include "common/axis.h"
 
-bool gyroSyncCheckUpdate(const gyro_t *gyro)
+#include "sensors/gyro.h"
+
+static int mpuDividerDrops;
+static volatile bool isPidScheduledToRun;
+static int pidProcessDenom;
+uint32_t lastGyroInterruptCallDelta;
+
+bool pidScheduledToRun(void)
 {
-    if (!gyro->intStatus)
-        return false;
-    return gyro->intStatus();
+    bool ret;
+    if (isPidScheduledToRun) {
+        ret = true;
+        isPidScheduledToRun= false;
+    } else {
+        ret = false;
+    }
+    return ret;
 }
 
-#define GYRO_LPF_256HZ      0
-#define GYRO_LPF_188HZ      1
-#define GYRO_LPF_98HZ       2
-#define GYRO_LPF_42HZ       3
-#define GYRO_LPF_20HZ       4
-#define GYRO_LPF_10HZ       5
-#define GYRO_LPF_5HZ        6
-#define GYRO_LPF_NONE       7
-
-uint32_t gyroSetSampleRate(uint8_t lpf, uint8_t gyroSyncDenominator)
+uint32_t gyroSetSamplingInterval(uint8_t lpf, uint8_t gyroSyncDenominator, uint8_t pidDenom)
 {
     int gyroSamplePeriod;
+    pidProcessDenom = pidDenom;
 
     if (lpf == GYRO_LPF_256HZ || lpf == GYRO_LPF_NONE) {
         gyroSamplePeriod = 125;
@@ -52,4 +58,29 @@ uint32_t gyroSetSampleRate(uint8_t lpf, uint8_t gyroSyncDenominator)
 uint8_t gyroMPU6xxxGetDividerDrops(void)
 {
     return mpuDividerDrops;
+}
+
+void gyroHandleInterrupt(void) {
+	static int16_t gyroADCRaw[XYZ_AXIS_COUNT];
+	static int pidProcessCountDown;
+
+//#ifdef DEBUG_MPU_DATA_READY_INTERRUPT
+    static uint32_t lastGyroInterruptCallAt = 0;
+    uint32_t now = micros();
+    lastGyroInterruptCallDelta = now - lastGyroInterruptCallAt;
+    debug[0] = lastGyroInterruptCallDelta;
+    lastGyroInterruptCallAt = now;
+
+    if (!gyro.read(gyroADCRaw)) {
+        return;
+    }
+
+    processGyroData(gyroADCRaw);
+
+    if (pidProcessCountDown) {
+        pidProcessCountDown--;
+    } else {
+        pidProcessCountDown = pidProcessDenom - 1;
+        isPidScheduledToRun = true;
+    }
 }

src/main/flight/pid.c
@@ -74,7 +74,7 @@ pidControllerFuncPtr pid_controller = pidBetaflight; // which pid controller are
 
 void setTargetPidLooptime(uint8_t pidProcessDenom)
 {
-    targetPidLooptime = gyro.targetLooptime * pidProcessDenom;
+    targetPidLooptime = gyro.gyroSamplingInterval * pidProcessDenom;
 }
 
 void pidResetErrorGyroState(void)